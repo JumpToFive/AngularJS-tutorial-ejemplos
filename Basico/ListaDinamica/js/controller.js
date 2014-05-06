15b38c20672ee912cf4f812bcf954b2895added2
Luis Sebastián <l.sebastian@concentraonline.com>
2014-05-06 13:51:33 +0200

    Translating the examples to English

--- Basico/ListaDinamica/js/controller.js
+++ Basico/ListaDinamica/js/controller.js
@@ -1,12 +1,12 @@
 
 function controller($scope) {
-    //Inicializamos la lista con dos elementos
-    $scope.lista=["Manzanas","Pepinillos"];
+    //Initialize the elements in the list
+    $scope.list=["Apples","Pepinillos"];
 
     $scope.submit = function(message) {
-        //Anyadimos el elemento a la lista.
-        $scope.lista.push(message)
-        //Vaciamos el campo de texto
+        //Add the element to the list
+        $scope.list.push(message)
+        //Empty the input text
         $scope.message="";
     }
 }
